61b25b555e8e5b35b6b711e42263a2bc96a2a890
raymichaud <rmichaud@androscogginbank.com>
2016-04-29 17:09:11 -0400

    Add Bio, Work, and some Education to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,123 @@
+var bio = {
+  "name": "Ray A Michaud",
+  "role": "Web Developer",
+  "welcomeMessage": "Welcome. I am currently employed as a full stack web developer. I am pursuing educational credentials to augment my self-learning and experience in this field.",
+  "contacts": {
+    "phones": {
+      "mobile": "(207) 754-4930",
+      "work": "(207) 376-3516"
+    },
+    "email": {
+      "home": "rayamichaud@twc.com",
+      "work": "rmichaud@androscogginbank.com"
+    },
+    "github": "raymichaud",
+    "linkedin": "http://linkedin.com/in/ray-michaud-939b7235",
+    "degreed":"https://degreed.com/raymichaud/",
+    "location": "Lewiston, ME",
+    "picture": "images/RayMichaud.jpg",
+    "skills": [
+      "web application development",
+      "fine art painting"
+    ]
+  }
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "St Dominic Regional High School",
+      "location": "Lewiston, Mainframe",
+      "major": "College preparatory",
+      "dates": "1971-1976",
+      "graduated": "1976",
+      "url": "http://www.stdomsmaine.org/"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "school": "Udacity",
+      "dates": "2016-Present",
+      "courses": [
+        {
+          "title": "Front End Developer Nanodegree",
+          "url": "www.udacity.com"
+        },
+        {
+
+        }
+      ]
+
+
+    },
+    {
+      "title": "",
+      "school": "Code School",
+      "dates": "2013-Present",
+      "url": "",
+    }
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Androscoggin Bank",
+      "dates": "1996-Present",
+      "location": "30 Lisbon St, Lewiston, ME",
+      "positions": [
+        {
+          "title": "AVP/Web Development & Support Mgr",
+          "dates": "2010-Present",
+          "description": "Oversee the development of the bank's public website and intranet"
+        },
+        {
+          "title": "AVP/IS Development & Support Officer",
+          "dates": "2003-2010",
+          "description": "Create web-based solutions for reporting challenges"
+        },
+        {
+          "title": "Data Systems Officer",
+          "dates": "1999-2003",
+          "description": "Oversee mainframe operations and liaise with other operational departments"
+        },
+        {
+          "title": "Mainframe Systems Specialist",
+          "dates": "1999",
+          "description": "Oversee team of mainframe operators"
+        },
+        {
+          "title": "Mainframe Operator",
+          "dates": "1998-1999",
+          "description": "Perform daily mainframe operations"
+        },
+        {
+          "title": "Teller/Floating Customer Service Representative",
+          "dates": "1996-1998",
+          "description": "Perform teller functions, Account openings and closings, and IRA setups"
+        }
+      ]
+    },
+    {
+      "employer": "Bradlees, Inc.",
+      "dates": "1975-1996",
+      "location": "1 Bradlees Circle, Braintree, MA",
+      "positions": [
+        {
+          "title": "Customer Service Manager, Westbrook, ME",
+          "dates": "1988-1996",
+          "description": "Oversee customer service functions, oversee tellers, act as personnel manager, serve as northern New England technology trainer"
+        },
+        {
+          "title": "Sales Associate",
+          "dates": "1975-1988",
+          "description": "Process stock, act as cashier, act as front-end supervisor, act as cash office clerk, act as cash office trainer for Northern New England"
+        }
+      ]
+    }
+  ]
+};
+
+var project = {
+
+};
